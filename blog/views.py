4b17bbe2451a827a0490acb8afdb5b20a4e263f1
ananaysrivastava5 <ananaysrivastava5@gmail.com>
2025-01-17 13:48:25 +0530

    remove commented code

--- blog/views.py
+++ blog/views.py
@@ -70,32 +70,3 @@ class BlogViewSet(viewsets.ModelViewSet):
             'errors': False,
         }
         return Response(context)
-
-# class BlogView(APIView):
-#     authentication_classes = [JWTAuthentication]
-#     permission_classes = [IsAuthenticated]
-
-    
-#     def get_object(self, pk):
-#         try:
-#             return Blog.objects.get(pk=pk)
-#         except Blog.DoesNotExist:
-#             raise Http404
-    
-#     def get(self, request, pk, format=None):
-#         blog = self.get_object(pk)
-#         serializer = SnippetSerializer(snippet)
-#         return Response(serializer.data)
-
-#     def put(self, request, pk, format=None):
-#         snippet = self.get_object(pk)
-#         serializer = SnippetSerializer(snippet, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-#     def delete(self, request, pk, format=None):
-#         snippet = self.get_object(pk)
-#         snippet.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
